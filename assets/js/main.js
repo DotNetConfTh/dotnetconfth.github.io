42e5fc4c5ec7b50e0bc6fd6b37e4ad40ad35c506
kidchenko <kidchenko@gmail.com>
2018-10-06 16:50:16 +0700

    Add pictues and mail list

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
     $(".countdown")
         .countdown("2018/10/27", function (event) {
             $(this).html(
-                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+                event.strftime('<div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
         });
 
