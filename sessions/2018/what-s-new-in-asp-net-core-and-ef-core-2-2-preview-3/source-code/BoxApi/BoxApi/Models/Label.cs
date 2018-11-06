4d35d86937bdbca38967a2b6e52503cbf10eebbe
aaronamm <theeranitp@gmail.com>
2018-11-07 00:58:58 +0700

    source codes => source code add EF source code

---
+++ sessions/2018/what-s-new-in-asp-net-core-and-ef-core-2-2-preview-3/source-code/BoxApi/BoxApi/Models/Label.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxApi.Models
+{
+  //[Owned]
+  public class Label
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string Color { get; set; }
+  }
+}
