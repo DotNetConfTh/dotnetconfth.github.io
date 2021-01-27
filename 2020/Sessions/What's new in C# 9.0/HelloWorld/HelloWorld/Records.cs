85b33e68a64501c8b0e64a75347d2deb1f9ca561
Will Fuqua <wafuqua@gmail.com>
2021-01-27 17:42:12 +0700

    add "What's new in C# 9.0" presentation

---
+++ 2020/Sessions/What's new in C# 9.0/HelloWorld/HelloWorld/Records.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HelloWorld
+{
+    public class Currency
+    {
+        public Currency(decimal amount, string currencyCode)
+        {
+            Amount = amount;
+            CurrencyCode = currencyCode;
+        }
+
+        public decimal Amount { get; }
+        public string CurrencyCode { get; }
+    }
+}
---
+++ 2020/Sessions/What's new in C# 9.0/HelloWorld/HelloWorld3.1/Records.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld3_1
+{
+    public record Currency(decimal Amount, string CurrencyCode);
+}
+
+
+
