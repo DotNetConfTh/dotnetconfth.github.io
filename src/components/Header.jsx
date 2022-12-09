9db2c5277ebc6d155545d14fe9118d17d4c22150
kidchenko <kidchenko@gmail.com>
2022-12-09 22:47:54 +0700

    change venue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,10 @@ export function Header() {
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
             <p>
-              <time dateTime="2022-04-06">17 of December, 2022</time>
+              <time dateTime="2022-12-17">17 of December, 2022</time>
             </p>
             <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
-            <a href='https://g.page/TrueDigitalPark' target="_blank" rel="noreferrer">True Digital Park - Bangkok</a>
+            <a href='https://goo.gl/maps/KCzvJzKEahJSVW4S6' target="_blank" rel="noreferrer">Glowfish Sathorn - Bangkok</a>
           </div>
         </div>
         <div className="hidden sm:mt-10 sm:flex lg:mt-0 lg:grow lg:basis-0 lg:justify-end">
--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -21,21 +21,32 @@ export function Venue() {
   return (
     <section id="sponsors" aria-label="Sponsors" className="py-20 sm:py-32">
       <Container>
-        <div className="mx-auto max-w-2xl lg:mx-0">
+        <div className="mx-auto max-w-2xl lg:mx-0 text-blue-900">
           <h2
             id="speakers-title"
             className="font-display text-4xl font-medium tracking-tighter text-blue-600 sm:text-5xl"
           >
             Venue
           </h2>
-          <p className="mt-4 font-display text-2xl tracking-tight text-blue-900">
-          Auditorium and Open Space - True Digital Park
+          <p className="text-blue-900 mt-3">
+            This year the conference will be hosted at Glowfish Sathorn.
           </p>
-          <address>
-            <a href="https://goo.gl/maps/pDbYcjDr8mm2ezYr8" target="_blank" rel="noreferrer">Address 101 Sukhumvit Road, Bangkok 10260 Thailand</a>
+          <p className="mt-3 font-display text-2xl tracking-tight text-blue-900">
+            2ND FLOOR, SATHORN THANI 2 - Glowfish Sathorn
+          </p>
+          <p className="mt-3">
+            Glowfish Sathorn right next to BTS Chong Nonsi and a walkable distance to MRT Saladaeng, 
+            Sathorn Thani building is part of Bangkok’s bustling business district. 
+            </p>
+          <p className="mt-3 text-sm">
+            TEL: +66 2 109 9600
+            INFO@GLOWFISHOFFICES.COM
+          </p>
+          <address className="mt-3">
+            <a href="https://goo.gl/maps/639mrsjVYUM6kUre8" target="_blank" rel="noreferrer">Address: 92/4, Floor 2, Sathorn Thani 2 Building, North Sathorn Road, Silom, Bang Rak, Bangkok 10500</a>
           </address>
           <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7753.176418078778!2d100.60927709231265!3d13.682785583493644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ed269181bb1%3A0x60c3178ba983c76!2sTrue%20Digital%20Park!5e0!3m2!1sen!2shk!4v1670423449830!5m2!1sen!2shk" height="450" width="100%" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21536.146820953672!2d100.51858286303988!3d13.724749242601424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29f2d589b4cf7%3A0xdcb09e862cf3258a!2sGLOWFISH!5e0!3m2!1sen!2sth!4v1670598995637!5m2!1sen!2sth" height="450" width="100%" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </Container>
